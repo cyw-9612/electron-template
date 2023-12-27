19de20457e5b58be144e642632351867115476a8
Solitario119 <1412385393@qq.com>
2023-12-27 12:16:25 +0800

    Initial commit

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,15 @@
+<!--
+ * @Author: Reny
+ * @Date: 2023-12-27 10:10:15
+ * @LastEditors: Solitario119 1412385393@qq.com
+ * @LastEditTime: 2023-12-27 11:11:51
+ * @FilePath: \electron-template\src\renderer\src\App.vue
+ * @Description: 
+-->
+<template>
+  <div m-3>分为服务范围服务费</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
